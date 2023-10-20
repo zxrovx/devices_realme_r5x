2c69091258f141de1e5105cda1812b5852268ff3
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2023-10-20 21:59:50 +0700

    Remove prebuilts/Muzix

--- device.mk
+++ device.mk
@@ -380,10 +380,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Muzix
-PRODUCT_PACKAGES += \
-    Muzix
-
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
